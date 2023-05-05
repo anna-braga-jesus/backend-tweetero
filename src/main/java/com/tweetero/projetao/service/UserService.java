e4d56740f10ee85321159d7cec8c56c22e3f5f12
anna-braga-jesus <annajesus.nave@gmail.com>
2023-05-05 18:31:09 -0300

    feat: add signup

--- src/main/java/com/tweetero/projetao/service/UserService.java
+++ src/main/java/com/tweetero/projetao/service/UserService.java
@@ -1,25 +1,20 @@
 package com.tweetero.projetao.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tweetero.projetao.dto.UserDTO;
-import com.tweetero.projetao.models.User;
+import com.tweetero.projetao.models.UserModel;
 import com.tweetero.projetao.repository.UserRepository;
+
 
 @Service
 public class UserService {
-
+    
     @Autowired
     private UserRepository repository;
 
-    public void singUp(UserDTO data ){
-        repository.save(new User(data));
-        }
-
-        public List<User> findAll(){
-            return repository.findAll();
-        }
+    public void signUp(UserDTO req){
+        repository.save(new UserModel(req));
+    }
 }
