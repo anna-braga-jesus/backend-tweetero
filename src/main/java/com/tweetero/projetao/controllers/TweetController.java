2e1662404fafc79b4ec1bc935bebd5738fa9c2e2
anna-braga-jesus <annajesus.nave@gmail.com>
2023-05-07 14:13:23 -0300

    feat: add get tweet with pagination

--- src/main/java/com/tweetero/projetao/controllers/TweetController.java
+++ src/main/java/com/tweetero/projetao/controllers/TweetController.java
@@ -1,32 +1,45 @@
 package com.tweetero.projetao.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweetero.projetao.dto.TweetDTO;
+import com.tweetero.projetao.models.TweetModel;
 import com.tweetero.projetao.service.TweetService;
 
 import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping("/api/tweetero")
+@RequestMapping("/api/tweets")
 public class TweetController {
 
     @Autowired
     private TweetService service;
 
-    @PostMapping("/tweet")
+    @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public String createATweet(@RequestBody @Valid TweetDTO req) {                
         service.createATweet(req); 
         return "Ok";
     }
+
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    public Page<TweetModel> findAll(@PageableDefault(page = 0, size = 5) @RequestParam Pageable page) {
+        return service.findAll(page);
+    }
+    
     
 }
--- src/main/java/com/tweetero/projetao/repository/TweetRepository.java
+++ src/main/java/com/tweetero/projetao/repository/TweetRepository.java
@@ -1,11 +1,13 @@
 package com.tweetero.projetao.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tweetero.projetao.models.TweetModel;
 
-public interface TweetRepository extends JpaRepository<TweetModel, Long>{
-    
-}
-
-
+public interface TweetRepository extends JpaRepository<TweetModel, Long> {
+    public List<TweetModel> findByUsername(String username);
+    Page<TweetModel> findAll(Pageable pageable);}
